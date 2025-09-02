d55c10772aa7c36b75470a222de2f1557fc11e2b
sai <sai_prasad_veluri@yahoo.com>
2025-09-02 11:43:46 +0000

    if else with customer Data without boolean variable

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CustomerModel } from 'src/customerModel';
 
 @Component({
   selector: 'app-customer',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class CustomerComponent {
    custname:string="John";
+// customers:CustomerModel[]=[
+// new CustomerModel(1,"Ankita","Pune"),
+// new CustomerModel(2,"Sunita","Chennai"),
+// new CustomerModel(3,"Ambika","Bangalore")
+// ];
+
+customers:CustomerModel[]=[];
+
 }
---
+++ src/customerModel.ts
@@ -0,0 +1,14 @@
+export class CustomerModel
+{
+    custid:number;
+custname:string;
+custcity:string;
+constructor(p_custid:number,p_name:string,p_city:string){
+this.custid=p_custid;
+this.custname=p_name;
+this.custcity=p_city;
+
+}
+
+
+}
